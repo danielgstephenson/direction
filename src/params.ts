4a653a460ebc040ccf479924468849a534894703
danielgstephenson <dgstephenson.econ@gmail.com>
2025-08-26 23:16:40 -0400

    rounds

--- src/params.ts
+++ src/params.ts
@@ -4,9 +4,9 @@ export const gridSize = 5
 export const unitCount = 6
 export const updateInterval = 0.2
 export const moveInterval = 0.5
-export const choiceInterval = 4
+export const choiceInterval = 3
 export const endInterval = 10
-export const maxRound = 40
+export const maxRound = 80
 export const moveVectors = [
   { x: 1, y: 0 },
   { x: 0, y: 1 },
